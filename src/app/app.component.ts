b521b65aa441e1ca1833e545e243ef923799875d
Alan Hauser <diefthyntis@gmail.com>
2024-09-17 13:04:41 +0200

    Ajout de 3 formulaires de type Template dans appComponent.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Form, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'FormTwoWayBinding';
+export class AppComponent{
+OnRecordProfile(formProfile:NgForm) {
+  console.log(formProfile.value);
+}
+
+
+
+  tsEmailAddress: string="luc@evangile.com";
+  tsPseudo: string="luc le porteur de bonne nouvelle";
+
+      title = 'FormTwoWayBinding';
+tsFirstname!: string;
+tsLastname!: string;
+tsBirthdate!: string;
+
+  OnRecordFirstname() {
+    console.log(this.tsFirstname);
+}
+  OnSignin() {
+  console.log(this.tsEmailAddress);
+  }
 }
